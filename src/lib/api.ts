fe6ba6caaf41fa2171de447c44f4bbed6f8ccb57
maeirnoam <61051073+maeirnoam@users.noreply.github.com>
2025-10-18 22:50:11 +0300

    testing chat

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const N8N_CHAT_WEBHOOK_URL = 'https://n8n.srv974700.hstgr.cloud/webhook/bf4dd093-bb02-472c-9454-7ab9af97bd1d';
+const N8N_CHAT_WEBHOOK_URL = 'https://n8n.srv974700.hstgr.cloud/webhook-test/bf4dd093-bb02-472c-9454-7ab9af97bd1d';
 const N8N_SUBMISSION_WEBHOOK_URL = 'https://n8n.srv974700.hstgr.cloud/webhook/310c4127-c569-4a9c-b96d-b44345627107';
 const N8N_REVIEW_WEBHOOK_URL = 'https://n8n.srv974700.hstgr.cloud/webhook/2cc0a4f3-1178-4fd1-8c9c-a5597f17d4d2';
 
